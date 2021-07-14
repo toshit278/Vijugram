3c7350f3ce96e8bc2b71e8a8fe3a406b7e35ce9e
toshit <toshit2780@gmail.com>
2021-07-14 20:25:02 +0530

    back

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.connect(MONGOURI)
 //     useNewUrlParser: true,
 //     useUnifiedTopology:true
 // })
-mongoose.connect(process.env.MONGOURI, {
+mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology:true
 })
@@ -34,20 +34,20 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 //bartaskar
 
-// if(process.env.NODE_ENV=="production"){
-//     app.use(express.static('client/build'))
-//     const path =require('path')
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path =require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
-const path =require('path')
-app.use(express.static(path.join(__dirname,"client","build")))
+// const path =require('path')
+// app.use(express.static(path.join(__dirname,"client","build")))
 
-app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,'client','build','index.html'));
-    });
+// app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname,'client','build','index.html'));
+//     });
 
 app.listen(PORT, () => {
     console.log("server is running on",PORT)
